440498b412ef235bfbe2c7d2f4e5bbaff4d5cac1
Taha <mrjoker773k@gmail.com>
2026-09-06 21:00:32 -0400

    Adding the reading from input stream and writing to the output stream in an inf loop (continue)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,10 +23,20 @@ public class Main {
                 try {
                     java.io.BufferedReader in = new java.io.BufferedReader(new java.io.InputStreamReader(clientSocket.getInputStream()));
                     java.io.OutputStream out = clientSocket.getOutputStream();
+
+                    String inputLine;
+                    // Keep reading as long as the client is connected
+                    while ((inputLine = in.readLine()) != null) {
+                        // For now, print what the client sent so you can see the RESP format
+                        System.out.println("Received: " + inputLine);
+
+                        // If we receive the PING command
+                        if (inputLine.equalsIgnoreCase("PING")) {
+                            out.write("+PONG\r\n".getBytes());
+                            out.flush();
+                        }
+                    }
                 }
-                
-                // Hardcode Pong response
-                clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
                 
                 // Close the client connection after responding
                 clientSocket.close();
